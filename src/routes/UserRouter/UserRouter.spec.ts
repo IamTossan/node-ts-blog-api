62854758d0b521f5a9183034a0c499cd7e52cc58
Albert Oudompheng <tossanmusic@gmail.com>
2018-07-09 00:32:14 +0200

    [test]: added superagent package + basic user creation unit test

--- src/routes/UserRouter/UserRouter.spec.ts
+++ src/routes/UserRouter/UserRouter.spec.ts
@@ -1,5 +1,8 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import * as superagent from 'superagent';
+
+import * as server from '../../index';
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -9,7 +12,34 @@ import { UserRouter } from './UserRouter';
 const instance = new UserRouter();
 
 describe('UserRouter', () => {
+
+    beforeEach((done) => {
+        server.boot(() => {
+            done();
+        });
+    });
+
+    afterEach((done) => {
+        server.shutdown(() => {
+            done();
+        });
+    });
+
     it('should be ok', () => {
         return instance.getUsers().should.be.fulfilled;
+    });
+
+    it('should create a new user', (done) => {
+        superagent.post('localhost:3000/api/user')
+            .send({
+                name: 'testname',
+                username: 'testusername',
+                email: 'testemail@test.com',
+                password: 'testpassword',
+            })
+            .then((res) => {
+                chai.assert(res.status === 201);
+                done()
+            });
     });
 });
